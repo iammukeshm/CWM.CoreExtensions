8da4daa7437c033ecc07edb585f20da9119da52e
Mukesh Murugan <iammukeshm@gmail.com>
2020-05-29 15:05:39 +0530

    Add project files.

---
+++ CWM.CoreExtensions/DatatableExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CWM.CoreExtensions
+{
+    public static class DatatableExtensions
+    {
+
+        public static List<T> ToList<T>(this DataTable dataTable)
+        {
+
+            var columnNames = dataTable.Columns.Cast<DataColumn>()
+                .Select(c => c.ColumnName)
+                .ToList();
+            var properties = typeof(T).GetProperties();
+            return dataTable.AsEnumerable().Select(row =>
+            {
+                var objT = Activator.CreateInstance<T>();
+                foreach (var pro in properties)
+                {
+                    if (columnNames.Contains(pro.Name))
+                    {
+                        PropertyInfo pI = objT.GetType().GetProperty(pro.Name);
+                        pro.SetValue(objT, row[pro.Name] == DBNull.Value ? null : Convert.ChangeType(row[pro.Name], pI.PropertyType));
+                    }
+                }
+                return objT;
+            }).ToList();
+
+
+        }
+    }
+}
---
+++ CWM.CoreExtensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CWM.CoreExtensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+    }
+}
